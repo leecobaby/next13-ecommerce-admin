20600e77144ac48180d3bb30a5cb521d6c1b951a
Leeco <leeco19@qq.com>
2023-08-15 13:39:43 +0800

    feat: create modal component

---
+++ app/(root)/page.tsx
@@ -0,0 +1,15 @@
+'use client'
+
+import Modal from '@/components/ui/modal'
+
+const SetupPage = () => {
+  return (
+    <div className="p-4">
+      <Modal title="test" description="test des" isOpen onClose={() => {}}>
+        13
+      </Modal>
+    </div>
+  )
+}
+
+export default SetupPage
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,9 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { ClerkProvider } from '@clerk/nextjs'
+
+import { ModalProvider } from '@/providers/modal-provider'
+import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,8 +14,13 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body className={inter.className}>
+          <ModalProvider />
+          {children}
+        </body>
+      </html>
+    </ClerkProvider>
   )
 }
